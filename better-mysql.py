efb3ee8c651f032cd8b00223300ac2e64c87a265
Bas <mega@ioexception.at>
2015-03-23 21:02:25 +0100

    Fixed some issues with the gauges being mixed up

--- better-mysql.py
+++ better-mysql.py
@@ -6,7 +6,7 @@ from amonagent.modules.plugins import AmonPlugin
 
 class BetterMySQLPLugin(AmonPlugin):
 
-    VERSION = '0.1.1'
+    VERSION = '0.2'
 
     # Tracked for 'per second'
     TRACKED = [
@@ -15,17 +15,17 @@ class BetterMySQLPLugin(AmonPlugin):
         'Qcache_hits',
     ]
 
-    GUAGES = {
-        'Threads_connected': 'net.connections',
+    GAUGES = {
+        'Threads_connected': 'info.connections',
 
         # Tracked variables
-        'Tracked_Bytes_sent': 'net.bytes.per.second',
-        'Tracked_Queries': 'performance.queries.per.second',
-        'Tracked_Qcache_hits': 'performance.cache.hits.per.second',
+        'Tracked_Bytes_sent':  'net.bytes_per_second',
+        'Tracked_Queries':     'performance.queries_per_second',
+        'Tracked_Qcache_hits': 'performance.cache_hits_per_second',
     }
 
     COUNTERS = {
-        'Connections': 'net.total.connections',
+        'Connections': 'net.total_connections',
     }
 
     def _connect(self):
@@ -52,7 +52,7 @@ class BetterMySQLPLugin(AmonPlugin):
         file = self.config.get('cache_file', '/tmp/amon-better-mysql')
         if not path.exists(file):
             open(file, 'a').close()  # Touches file
-            chmod(file, 0600)
+            chmod(file, 0700)
         return file
 
     def _cache_load(self):
@@ -66,7 +66,7 @@ class BetterMySQLPLugin(AmonPlugin):
     def _cache_save(self, data):
         file = self.config.get('cache_file', '/tmp/amon-better-mysql')
 
-        content = '\n'.join('%s|%i' % (k, v)
+        content = '\n'.join('%s|%i' % (k, int(v))
                             for k, v in data.items()
                             if k in self.TRACKED)
         content += '\ncache_timestamp|%i' % int(time())
@@ -75,35 +75,36 @@ class BetterMySQLPLugin(AmonPlugin):
             f.write(content)
 
     def collect(self):
-        self._connect()
-        self._cache_init()
+            self._connect()
+            self._cache_init()
 
-        self.cursor.execute("SHOW /*!50002 GLOBAL */ STATUS;")
-        results = dict(self.cursor.fetchall())
-        prev_cache = self._cache_load()
+            self.cursor.execute("SHOW /*!50002 GLOBAL */ STATUS;")
+            results = dict(self.cursor.fetchall())
+            prev_cache = self._cache_load()
 
-        if isinstance(prev_cache, dict):
-            time_diff = int(time()) - prev_cache['cache_timestamp'] or 1
+            if isinstance(prev_cache, dict):
+                time_diff = int(time()) - prev_cache['cache_timestamp'] or 1
 
-        # Fetch stuff that is up for grabs
-        for k, v in results.items():
-            if k in self.TRACKED and isinstance(prev_cache, dict):
-                per_sec = (int(v) - prev_cache[k]) // time_diff
-                self.gauge(self.GAUGES['Tracked_' + k], per_sec)
-            if k in self.COUNTERS:
-                self.counter(self.COUNTERS[k], v)
-            if k in self.GAUGES:
-                self.gauge(self.GAUGES[k], v)
+            # Fetch stuff that is up for grabs
+            for k, v in results.items():
+                if k in self.TRACKED and isinstance(prev_cache, dict):
+                    per_sec = (int(v) - prev_cache[k]) // time_diff
+                    self.gauge(self.GAUGES['Tracked_' + k], per_sec)
+                if k in self.COUNTERS:
+                    self.counter(self.COUNTERS[k], v)
+                if k in self.GAUGES:
+                    self.gauge(self.GAUGES[k], v)
 
-        self._cache_save(results)
+            self._cache_save(results)
 
-        self.cursor.execute("SELECT VERSION();")
-        try:
-            mysql_version = self.cursor.fetchone()[0]
-        except:
-            mysql_version = None
+            self.cursor.execute("SELECT VERSION();")
+            try:
+                mysql_version = self.cursor.fetchone()[0]
+            except:
+                mysql_version = None
 
-        self.version(mysql=mysql_version,
-                plugin=self.VERSION,
-                mysqldb=MySQLdb.__version__)
-        self._disconnect()
+            self.version(mysql=mysql_version,
+                    plugin=self.VERSION,
+                    mysqldb=MySQLdb.__version__)
+
+            self._disconnect()
